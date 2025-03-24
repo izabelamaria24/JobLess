43153e176cde74d3e921512aedf23c297c04d8f6
Iza <izabelajilavu@gmail.com>
2025-03-24 23:07:50 +0200

    profile page created

--- frontend/src/pages/Profile.js
+++ frontend/src/pages/Profile.js
@@ -1,8 +1,27 @@
+import React, { useContext } from 'react';
+import { AuthContext } from '../context/AuthContext';
+import '../design/Profile.css';
+
 const Profile = () => {
+    const { user } = useContext(AuthContext);
+
+    if (!user) {
+        return <p>Loading...</p>;
+    }
+
     return (
-        <>
-            
-        </>
+        <div className="profile-page">
+            <h2>User Profile</h2>
+            <div className="profile-details">
+                <p><strong>Username:</strong> {user.username}</p>
+                <p><strong>First Name:</strong> {user.firstname}</p>
+                <p><strong>Last Name:</strong> {user.lastname}</p>
+                <p><strong>Email:</strong> {user.email}</p>
+                <p><strong>Phone Number:</strong> {user.phone_number}</p>
+            </div>
+            <button className="edit-profile-button">Edit Profile</button>
+        </div>
     );
 };
+
 export default Profile;
