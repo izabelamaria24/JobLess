0f5c5c8af2e490bb6aa93c012388167b1c65996b
Daria <82290545+dariadragomir@users.noreply.github.com>
2025-06-01 22:29:42 +0300

    Update test_add_response_button.py

--- automated_tests/test_add_response_button.py
+++ automated_tests/test_add_response_button.py
@@ -2,7 +2,7 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 
 def test_upload_cv_button(driver):
-    driver.get("https://example.com")
+    driver.get("http://localhost:3000/applications/1")  # test on application with id 1
 
     # test form for adding response appears when clicking 'Add esponse' button
     about_link = driver.find_element("xpath", '//a[text()="Add Response"]')
