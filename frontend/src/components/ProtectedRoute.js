7e7119e02577cf843bb13399a882b385ffd6a8c1
Iza <izabelajilavu@gmail.com>
2025-06-01 23:55:38 +0300

    compare salaries design

--- frontend/src/components/ProtectedRoute.js
+++ frontend/src/components/ProtectedRoute.js
@@ -3,14 +3,14 @@ import { useContext } from "react";
 import { AuthContext } from "../context/AuthContext";
 
 const ProtectedRoute = ({ children }) => {
-    const { token, loading } = useContext(AuthContext);
-    if (loading) {
-        return <div>Loading...</div>;
-    }
+    // const { token, loading } = useContext(AuthContext);
+    // if (loading) {
+    //     return <div>Loading...</div>;
+    // }
 
-    if (!token) {
-        return <Navigate to="/login" />;
-    }
+    // if (!token) {
+    //     return <Navigate to="/login" />;
+    // }
 
     return children;
 };
